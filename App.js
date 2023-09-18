6253d01d6ef5fe63d5df855cc2afe3c8a58d5cfc
SankalpSunagar <145355622+SankalpSunagar@users.noreply.github.com>
2023-09-18 19:28:35 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Disperse from './Disperse';
+
+function App() {
+  return (
+    <div className="App">
+      <Disperse />
+    </div>
+  );
+}
+
+export default App;
